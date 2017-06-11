8fabc298129b0ad4cc8dd012b144573f9b2bcaa0
Peter Murwa <wanjala.pet@gmail.com>
2017-06-11 20:27:00 +0300

    Add main module

--- dist/main.js
+++ dist/main.js
@@ -13,6 +13,17 @@ angular.module('angular-utility-filters.domain', [])
         }
     }])
 /**
+ * Main module - depends on all other submodules
+ * @copyright @murwa 2017
+ */
+angular.module('angular-utility-filters', [
+    'angular-utility-filters.str-limit',
+    'angular-utility-filters.domain',
+    'angular-utility-filters.uc-first',
+    'angular-utility-filters.search-encode',
+    'angular-utility-filters.uc-words',
+]);
+/**
  * @copyright @murwa 2017
  */
 angular.module('angular-utility-filters.search-decode', [])
---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * Main module - depends on all other submodules
+ * @copyright @murwa 2017
+ */
+angular.module('angular-utility-filters', [
+    'angular-utility-filters.str-limit',
+    'angular-utility-filters.domain',
+    'angular-utility-filters.uc-first',
+    'angular-utility-filters.search-encode',
+    'angular-utility-filters.uc-words',
+]);
