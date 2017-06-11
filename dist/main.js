a7f601d71d6fc1d01b7be5d1b8da156eb947189f
Peter Murwa <wanjala.pet@gmail.com>
2017-06-11 19:23:04 +0300

    Dist

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,49 @@
-
-
-
-
-
+angular.module('angular-utility-filters.domain', [])
+    .filter('domain', [function () {
+        return function (input, protocol) {
+            var parser = document.createElement('a');
+            if (protocol) {
+                input = protocol + input.replace(/www./, '');
+            }
+            parser.href = input;
+            return parser.hostname.replace(/^www./, '');
+        }
+    }])
+angular.module('angular-utility-filters.search-decode', [])
+    .filter('searchDecode', [function () {
+        return function (input) {
+            return typeof input === 'string' ? input.replace(/\+/gm, ' ') : input;
+        }
+    }]);
+angular.module('angular-utility-filters.search-encode', [])
+    .filter('searchEncode', [function () {
+        return function (input) {
+            return (typeof input === 'string' ? input.replace(/( )/gm, '+') : input).toLowerCase();
+        }
+    }])
+angular.module('angular-utility-filters.str-limit', [])
+    .filter('strLimit', ['$filter', function ($filter) {
+        return function (input, length, ellipse) {
+            if (!input || !angular.isString(input)) {
+                return null;
+            }
+            return $filter('limitTo')(angular.copy(input), length) + (input && input.length > length ? (ellipse || '...') : '');
+        }
+    }]);
+angular.module('angular-utility-filters.uc-first', [])
+    .filter('ucFirst', [function () {
+        return function (input) {
+            if (!input) {
+                return;
+            }
+            return input.charAt(0).toUpperCase() + input.slice(1)
+        }
+    }])
+angular.module('angular-utility-filters.uc-words', [])
+    .filter('ucWords', function () {
+        return function (input) {
+            return angular.isString(input) ? input.replace(/^([a-z])|[\s|\t|\r|\n|\-|\.]+([a-z])/g, function (a) {
+                return a.toUpperCase();
+            }) : input;
+        };
+    })
