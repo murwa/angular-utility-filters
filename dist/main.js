051cad3a820b0500b00cb0738d8d9e25c6eefaba
Peter Murwa <wanjala.pet@gmail.com>
2017-06-19 07:53:39 +0300

    Configurable search encode character

--- dist/main.js
+++ dist/main.js
@@ -44,9 +44,42 @@
  */
 (function (angular) {
     angular.module('angular-utility-filters.search-encode', [])
-        .filter('searchEncode', [function () {
+        .provider('$searchEncode', function () {
+            /**
+             * Default encode character
+             */
+            var encodeCharacter = '+';
+            /**
+             * Set encode character
+             * @param {string}
+             * @return {*}
+             */
+            this.setEncodeCharacter = function (value) {
+                encodeCharacter = value;
+                return this;
+            }
+
+            /**
+             * Provider's $get property
+             */
+            this.$get = [function () {
+                return new SearchEncodeService(encodeCharacter);
+                /**
+                 * Search endcode service
+                 *
+                 * @param {*} value 
+                 */
+                function SearchEncodeService(value) {
+                    this.getEncodeCharacter = function () {
+                        return value;
+                    }
+                }
+            }];
+
+        })
+        .filter('searchEncode', ['$searchEncode', function ($searchEncode) {
             return function (input) {
-                return (typeof input === 'string' ? input.replace(/( )/gm, '+') : input).toLowerCase();
+                return (typeof input === 'string' ? input.replace(/( )/gm, $searchEncode.getEncodeCharacter()) : input).toLowerCase();
             }
         }])
 })(angular);
